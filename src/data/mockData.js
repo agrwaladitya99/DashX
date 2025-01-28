6a54f7c4f721fe2b7db54a50dd334a45af6829ac
Aditya Agarwal <agaadi01@CSGICORP.COM>
2025-01-28 09:57:20 +0530

    DashX!

---
+++ src/data/mockData.js
@@ -0,0 +1,1289 @@
+import { tokens } from "../theme";
+
+export const mockDataTeam = [
+  {
+    id: 1,
+    name: "Professor",
+    email: "professor@gmail.com",
+    age: 50,
+    phone: "(100)000-0001",
+    access: "admin",
+  },
+  {
+    id: 2,
+    name: "Tokyo",
+    email: "tokyo@gmail.com",
+    age: 31,
+    phone: "(200)000-0002",
+    access: "manager",
+  },
+  {
+    id: 3,
+    name: "Berlin",
+    email: "berlin@gmail.com",
+    age: 40,
+    phone: "(300)000-0003",
+    access: "user",
+  },
+  {
+    id: 4,
+    name: "Nairobi",
+    email: "nairobi@gmail.com",
+    age: 38,
+    phone: "(400)000-0004",
+    access: "admin",
+  },
+  {
+    id: 5,
+    name: "Rio",
+    email: "rio@gmail.com",
+    age: 26,
+    phone: "(500)000-0005",
+    access: "user",
+  },
+  {
+    id: 6,
+    name: "Moscow",
+    email: "moscow@gmail.com",
+    age: 55,
+    phone: "(600)000-0006",
+    access: "manager",
+  },
+  {
+    id: 7,
+    name: "Helsinki",
+    email: "helsinki@gmail.com",
+    age: 42,
+    phone: "(700)000-0007",
+    access: "user",
+  },
+  {
+    id: 8,
+    name: "Denver",
+    email: "denver@gmail.com",
+    age: 29,
+    phone: "(800)000-0008",
+    access: "user",
+  },
+  {
+    id: 9,
+    name: "Stockholm",
+    email: "stockholm@gmail.com",
+    age: 27,
+    phone: "(900)000-0009",
+    access: "admin",
+  },
+];
+
+export const mockDataContacts = [
+  {
+    id: 1,
+    name: "Professor",
+    email: "professor@gmail.com",
+    age: 50,
+    phone: "(111)111-1111",
+    address: "1234 Brain Street, Madrid, ES 28001",
+    city: "Madrid",
+    zipCode: "28001",
+    registrarId: 123512,
+  },
+  {
+    id: 2,
+    name: "Tokyo",
+    email: "tokyo@gmail.com",
+    age: 31,
+    phone: "(222)222-2222",
+    address: "5678 Hero Avenue, Madrid, ES 28002",
+    city: "Madrid",
+    zipCode: "28002",
+    registrarId: 123512,
+  },
+  {
+    id: 3,
+    name: "Berlin",
+    email: "berlin@gmail.com",
+    age: 40,
+    phone: "(333)333-3333",
+    address: "91011 Gold Street, Madrid, ES 28003",
+    city: "Madrid",
+    zipCode: "28003",
+    registrarId: 4132513,
+  },
+  {
+    id: 4,
+    name: "Nairobi",
+    email: "nairobi@gmail.com",
+    age: 38,
+    phone: "(444)444-4444",
+    address: "12131 Mint Road, Madrid, ES 28004",
+    city: "Madrid",
+    zipCode: "28004",
+    registrarId: 123512,
+  },
+  {
+    id: 5,
+    name: "Rio",
+    email: "rio@gmail.com",
+    age: 26,
+    phone: "(555)555-5555",
+    address: "14151 Tech Street, Madrid, ES 28005",
+    city: "Madrid",
+    zipCode: "28005",
+    registrarId: 123512,
+  },
+  {
+    id: 6,
+    name: "Moscow",
+    email: "moscow@gmail.com",
+    age: 55,
+    phone: "(666)666-6666",
+    address: "16171 Heavy Road, Madrid, ES 28006",
+    city: "Madrid",
+    zipCode: "28006",
+    registrarId: 123512,
+  },
+  {
+    id: 7,
+    name: "Helsinki",
+    email: "helsinki@gmail.com",
+    age: 42,
+    phone: "(777)777-7777",
+    address: "18181 Strong Street, Madrid, ES 28007",
+    city: "Madrid",
+    zipCode: "28007",
+    registrarId: 123512,
+  },
+  {
+    id: 8,
+    name: "Denver",
+    email: "denver@gmail.com",
+    age: 29,
+    phone: "(888)888-8888",
+    address: "20202 Poppy Avenue, Madrid, ES 28008",
+    city: "Madrid",
+    zipCode: "28008",
+    registrarId: 512315,
+  },
+  {
+    id: 9,
+    name: "Stockholm",
+    email: "stockholm@gmail.com",
+    age: 27,
+    phone: "(999)999-9999",
+    address: "21212 Pink Boulevard, Madrid, ES 28009",
+    city: "Madrid",
+    zipCode: "28009",
+    registrarId: 928397,
+  },
+  {
+    id: 10,
+    name: "Arturo Roman",
+    email: "arturo@gmail.com",
+    age: 45,
+    phone: "(101)010-1010",
+    address: "22222 Corporate Way, Madrid, ES 28010",
+    city: "Madrid",
+    zipCode: "28010",
+    registrarId: 533215,
+  },
+  {
+    id: 11,
+    name: "Monica Gaztambide",
+    email: "monica@gmail.com",
+    age: 34,
+    phone: "(111)111-1112",
+    address: "23232 Love Lane, Madrid, ES 28011",
+    city: "Madrid",
+    zipCode: "28011",
+    registrarId: 92197,
+  },
+];
+
+export const mockDataInvoices = [
+  {
+    id: 1,
+    name: "Professor",
+    email: "professor@gmail.com",
+    cost: "1500.00",
+    phone: "(111)111-1111",
+    date: "01/15/2023",
+  },
+  {
+    id: 2,
+    name: "Tokyo",
+    email: "tokyo@gmail.com",
+    cost: "200.50",
+    phone: "(222)222-2222",
+    date: "02/10/2023",
+  },
+  {
+    id: 3,
+    name: "Berlin",
+    email: "berlin@gmail.com",
+    cost: "300.75",
+    phone: "(333)333-3333",
+    date: "03/05/2023",
+  },
+  {
+    id: 4,
+    name: "Nairobi",
+    email: "nairobi@gmail.com",
+    cost: "50.40",
+    phone: "(444)444-4444",
+    date: "04/20/2023",
+  },
+  {
+    id: 5,
+    name: "Rio",
+    email: "rio@gmail.com",
+    cost: "100.10",
+    phone: "(555)555-5555",
+    date: "05/25/2023",
+  },
+  {
+    id: 6,
+    name: "Moscow",
+    email: "moscow@gmail.com",
+    cost: "75.20",
+    phone: "(666)666-6666",
+    date: "06/30/2023",
+  },
+  {
+    id: 7,
+    name: "Helsinki",
+    email: "helsinki@gmail.com",
+    cost: "125.99",
+    phone: "(777)777-7777",
+    date: "07/15/2023",
+  },
+  {
+    id: 8,
+    name: "Denver",
+    email: "denver@gmail.com",
+    cost: "45.00",
+    phone: "(888)888-8888",
+    date: "08/22/2023",
+  },
+];
+
+export const mockTransactions = [
+  {
+    txId: "TX01",
+    user: "Professor",
+    date: "2021-09-01",
+    cost: "1500.00",
+  },
+  {
+    txId: "TX02",
+    user: "Tokyo",
+    date: "2022-04-01",
+    cost: "300.45",
+  },
+  {
+    txId: "TX03",
+    user: "Berlin",
+    date: "2021-09-01",
+    cost: "250.95",
+  },
+  {
+    txId: "TX04",
+    user: "Nairobi",
+    date: "2022-11-05",
+    cost: "500.00",
+  },
+  {
+    txId: "TX05",
+    user: "Rio",
+    date: "2022-11-02",
+    cost: "75.55",
+  },
+  {
+    txId: "TX06",
+    user: "Moscow",
+    date: "2021-09-01",
+    cost: "43.95",
+  },
+  {
+    txId: "TX07",
+    user: "Helsinki",
+    date: "2019-04-15",
+    cost: "24.20",
+  },
+  {
+    txId: "TX08",
+    user: "Denver",
+    date: "2022-04-01",
+    cost: "133.45",
+  },
+  {
+    txId: "TX09",
+    user: "Stockholm",
+    date: "2022-02-10",
+    cost: "60.00",
+  },
+];
+
+export const mockBarData = [
+  {
+    country: "AD",
+    "blue lagoon": 137,
+    "blue lagoonColor": "hsl(229, 70%, 50%)",
+    vodkaPasta: 96,
+    vodkaPastaColor: "hsl(296, 70%, 50%)",
+    mohito: 72,
+    mohitoColor: "hsl(97, 70%, 50%)",
+    cheeseCake: 140,
+    cheeseCakeColor: "hsl(340, 70%, 50%)",
+  },
+  {
+    country: "AE",
+    "blue lagoon": 55,
+    "blue lagoonColor": "hsl(307, 70%, 50%)",
+    vodkaPasta: 28,
+    vodkaPastaColor: "hsl(111, 70%, 50%)",
+    mohito: 58,
+    mohitoColor: "hsl(273, 70%, 50%)",
+    cheeseCake: 29,
+    cheeseCakeColor: "hsl(275, 70%, 50%)",
+  },
+  {
+    country: "AF",
+    "blue lagoon": 109,
+    "blue lagoonColor": "hsl(72, 70%, 50%)",
+    vodkaPasta: 23,
+    vodkaPastaColor: "hsl(96, 70%, 50%)",
+    mohito: 34,
+    mohitoColor: "hsl(106, 70%, 50%)",
+    cheeseCake: 152,
+    cheeseCakeColor: "hsl(256, 70%, 50%)",
+  },
+  {
+    country: "AG",
+    "blue lagoon": 133,
+    "blue lagoonColor": "hsl(257, 70%, 50%)",
+    vodkaPasta: 52,
+    vodkaPastaColor: "hsl(326, 70%, 50%)",
+    mohito: 43,
+    mohitoColor: "hsl(110, 70%, 50%)",
+    cheeseCake: 83,
+    cheeseCakeColor: "hsl(9, 70%, 50%)",
+  },
+  {
+    country: "AI",
+    "blue lagoon": 81,
+    "blue lagoonColor": "hsl(190, 70%, 50%)",
+    vodkaPasta: 80,
+    vodkaPastaColor: "hsl(325, 70%, 50%)",
+    mohito: 112,
+    mohitoColor: "hsl(54, 70%, 50%)",
+    cheeseCake: 35,
+    cheeseCakeColor: "hsl(285, 70%, 50%)",
+  },
+  {
+    country: "AL",
+    "blue lagoon": 66,
+    "blue lagoonColor": "hsl(208, 70%, 50%)",
+    vodkaPasta: 111,
+    vodkaPastaColor: "hsl(334, 70%, 50%)",
+    mohito: 167,
+    mohitoColor: "hsl(182, 70%, 50%)",
+    cheeseCake: 18,
+    cheeseCakeColor: "hsl(76, 70%, 50%)",
+  },
+  {
+    country: "AM",
+    "blue lagoon": 80,
+    "blue lagoonColor": "hsl(87, 70%, 50%)",
+    vodkaPasta: 47,
+    vodkaPastaColor: "hsl(141, 70%, 50%)",
+    mohito: 158,
+    mohitoColor: "hsl(224, 70%, 50%)",
+    cheeseCake: 49,
+    cheeseCakeColor: "hsl(274, 70%, 50%)",
+  },
+];
+
+export const mockPieData = [
+  {
+    id: "hack",
+    label: "hack",
+    value: 239,
+    color: "hsl(104, 70%, 50%)",
+  },
+  {
+    id: "make",
+    label: "make",
+    value: 170,
+    color: "hsl(162, 70%, 50%)",
+  },
+  {
+    id: "go",
+    label: "go",
+    value: 322,
+    color: "hsl(291, 70%, 50%)",
+  },
+  {
+    id: "lisp",
+    label: "lisp",
+    value: 503,
+    color: "hsl(229, 70%, 50%)",
+  },
+  {
+    id: "scala",
+    label: "scala",
+    value: 584,
+    color: "hsl(344, 70%, 50%)",
+  },
+];
+
+export const mockLineData = [
+  {
+    id: "japan",
+    color: tokens("dark").greenAccent[500],
+    data: [
+      {
+        x: "plane",
+        y: 101,
+      },
+      {
+        x: "helicopter",
+        y: 75,
+      },
+      {
+        x: "boat",
+        y: 36,
+      },
+      {
+        x: "train",
+        y: 216,
+      },
+      {
+        x: "subway",
+        y: 35,
+      },
+      {
+        x: "bus",
+        y: 236,
+      },
+      {
+        x: "car",
+        y: 88,
+      },
+      {
+        x: "moto",
+        y: 232,
+      },
+      {
+        x: "bicycle",
+        y: 281,
+      },
+      {
+        x: "horse",
+        y: 1,
+      },
+      {
+        x: "skateboard",
+        y: 35,
+      },
+      {
+        x: "others",
+        y: 14,
+      },
+    ],
+  },
+  {
+    id: "france",
+    color: tokens("dark").blueAccent[300],
+    data: [
+      {
+        x: "plane",
+        y: 212,
+      },
+      {
+        x: "helicopter",
+        y: 190,
+      },
+      {
+        x: "boat",
+        y: 270,
+      },
+      {
+        x: "train",
+        y: 9,
+      },
+      {
+        x: "subway",
+        y: 75,
+      },
+      {
+        x: "bus",
+        y: 175,
+      },
+      {
+        x: "car",
+        y: 33,
+      },
+      {
+        x: "moto",
+        y: 189,
+      },
+      {
+        x: "bicycle",
+        y: 97,
+      },
+      {
+        x: "horse",
+        y: 87,
+      },
+      {
+        x: "skateboard",
+        y: 299,
+      },
+      {
+        x: "others",
+        y: 251,
+      },
+    ],
+  },
+  {
+    id: "us",
+    color: tokens("dark").redAccent[200],
+    data: [
+      {
+        x: "plane",
+        y: 191,
+      },
+      {
+        x: "helicopter",
+        y: 136,
+      },
+      {
+        x: "boat",
+        y: 91,
+      },
+      {
+        x: "train",
+        y: 190,
+      },
+      {
+        x: "subway",
+        y: 211,
+      },
+      {
+        x: "bus",
+        y: 152,
+      },
+      {
+        x: "car",
+        y: 189,
+      },
+      {
+        x: "moto",
+        y: 152,
+      },
+      {
+        x: "bicycle",
+        y: 8,
+      },
+      {
+        x: "horse",
+        y: 197,
+      },
+      {
+        x: "skateboard",
+        y: 107,
+      },
+      {
+        x: "others",
+        y: 170,
+      },
+    ],
+  },
+];
+
+export const mockGeographyData = [
+  {
+    id: "AFG",
+    value: 520600,
+  },
+  {
+    id: "AGO",
+    value: 949905,
+  },
+  {
+    id: "ALB",
+    value: 329910,
+  },
+  {
+    id: "ARE",
+    value: 675484,
+  },
+  {
+    id: "ARG",
+    value: 432239,
+  },
+  {
+    id: "ARM",
+    value: 288305,
+  },
+  {
+    id: "ATA",
+    value: 415648,
+  },
+  {
+    id: "ATF",
+    value: 665159,
+  },
+  {
+    id: "AUT",
+    value: 798526,
+  },
+  {
+    id: "AZE",
+    value: 481678,
+  },
+  {
+    id: "BDI",
+    value: 496457,
+  },
+  {
+    id: "BEL",
+    value: 252276,
+  },
+  {
+    id: "BEN",
+    value: 440315,
+  },
+  {
+    id: "BFA",
+    value: 343752,
+  },
+  {
+    id: "BGD",
+    value: 920203,
+  },
+  {
+    id: "BGR",
+    value: 261196,
+  },
+  {
+    id: "BHS",
+    value: 421551,
+  },
+  {
+    id: "BIH",
+    value: 974745,
+  },
+  {
+    id: "BLR",
+    value: 349288,
+  },
+  {
+    id: "BLZ",
+    value: 305983,
+  },
+  {
+    id: "BOL",
+    value: 430840,
+  },
+  {
+    id: "BRN",
+    value: 345666,
+  },
+  {
+    id: "BTN",
+    value: 649678,
+  },
+  {
+    id: "BWA",
+    value: 319392,
+  },
+  {
+    id: "CAF",
+    value: 722549,
+  },
+  {
+    id: "CAN",
+    value: 332843,
+  },
+  {
+    id: "CHE",
+    value: 122159,
+  },
+  {
+    id: "CHL",
+    value: 811736,
+  },
+  {
+    id: "CHN",
+    value: 593604,
+  },
+  {
+    id: "CIV",
+    value: 143219,
+  },
+  {
+    id: "CMR",
+    value: 630627,
+  },
+  {
+    id: "COG",
+    value: 498556,
+  },
+  {
+    id: "COL",
+    value: 660527,
+  },
+  {
+    id: "CRI",
+    value: 60262,
+  },
+  {
+    id: "CUB",
+    value: 177870,
+  },
+  {
+    id: "-99",
+    value: 463208,
+  },
+  {
+    id: "CYP",
+    value: 945909,
+  },
+  {
+    id: "CZE",
+    value: 500109,
+  },
+  {
+    id: "DEU",
+    value: 63345,
+  },
+  {
+    id: "DJI",
+    value: 634523,
+  },
+  {
+    id: "DNK",
+    value: 731068,
+  },
+  {
+    id: "DOM",
+    value: 262538,
+  },
+  {
+    id: "DZA",
+    value: 760695,
+  },
+  {
+    id: "ECU",
+    value: 301263,
+  },
+  {
+    id: "EGY",
+    value: 148475,
+  },
+  {
+    id: "ERI",
+    value: 939504,
+  },
+  {
+    id: "ESP",
+    value: 706050,
+  },
+  {
+    id: "EST",
+    value: 977015,
+  },
+  {
+    id: "ETH",
+    value: 461734,
+  },
+  {
+    id: "FIN",
+    value: 22800,
+  },
+  {
+    id: "FJI",
+    value: 18985,
+  },
+  {
+    id: "FLK",
+    value: 64986,
+  },
+  {
+    id: "FRA",
+    value: 447457,
+  },
+  {
+    id: "GAB",
+    value: 669675,
+  },
+  {
+    id: "GBR",
+    value: 757120,
+  },
+  {
+    id: "GEO",
+    value: 158702,
+  },
+  {
+    id: "GHA",
+    value: 893180,
+  },
+  {
+    id: "GIN",
+    value: 877288,
+  },
+  {
+    id: "GMB",
+    value: 724530,
+  },
+  {
+    id: "GNB",
+    value: 387753,
+  },
+  {
+    id: "GNQ",
+    value: 706118,
+  },
+  {
+    id: "GRC",
+    value: 377796,
+  },
+  {
+    id: "GTM",
+    value: 66890,
+  },
+  {
+    id: "GUY",
+    value: 719300,
+  },
+  {
+    id: "HND",
+    value: 739590,
+  },
+  {
+    id: "HRV",
+    value: 929467,
+  },
+  {
+    id: "HTI",
+    value: 538961,
+  },
+  {
+    id: "HUN",
+    value: 146095,
+  },
+  {
+    id: "IDN",
+    value: 490681,
+  },
+  {
+    id: "IND",
+    value: 549818,
+  },
+  {
+    id: "IRL",
+    value: 630163,
+  },
+  {
+    id: "IRN",
+    value: 596921,
+  },
+  {
+    id: "IRQ",
+    value: 767023,
+  },
+  {
+    id: "ISL",
+    value: 478682,
+  },
+  {
+    id: "ISR",
+    value: 963688,
+  },
+  {
+    id: "ITA",
+    value: 393089,
+  },
+  {
+    id: "JAM",
+    value: 83173,
+  },
+  {
+    id: "JOR",
+    value: 52005,
+  },
+  {
+    id: "JPN",
+    value: 199174,
+  },
+  {
+    id: "KAZ",
+    value: 181424,
+  },
+  {
+    id: "KEN",
+    value: 60946,
+  },
+  {
+    id: "KGZ",
+    value: 432478,
+  },
+  {
+    id: "KHM",
+    value: 254461,
+  },
+  {
+    id: "OSA",
+    value: 942447,
+  },
+  {
+    id: "KWT",
+    value: 414413,
+  },
+  {
+    id: "LAO",
+    value: 448339,
+  },
+  {
+    id: "LBN",
+    value: 620090,
+  },
+  {
+    id: "LBR",
+    value: 435950,
+  },
+  {
+    id: "LBY",
+    value: 75091,
+  },
+  {
+    id: "LKA",
+    value: 595124,
+  },
+  {
+    id: "LSO",
+    value: 483524,
+  },
+  {
+    id: "LTU",
+    value: 867357,
+  },
+  {
+    id: "LUX",
+    value: 689172,
+  },
+  {
+    id: "LVA",
+    value: 742980,
+  },
+  {
+    id: "MAR",
+    value: 236538,
+  },
+  {
+    id: "MDA",
+    value: 926836,
+  },
+  {
+    id: "MDG",
+    value: 840840,
+  },
+  {
+    id: "MEX",
+    value: 353910,
+  },
+  {
+    id: "MKD",
+    value: 505842,
+  },
+  {
+    id: "MLI",
+    value: 286082,
+  },
+  {
+    id: "MMR",
+    value: 915544,
+  },
+  {
+    id: "MNE",
+    value: 609500,
+  },
+  {
+    id: "MNG",
+    value: 410428,
+  },
+  {
+    id: "MOZ",
+    value: 32868,
+  },
+  {
+    id: "MRT",
+    value: 375671,
+  },
+  {
+    id: "MWI",
+    value: 591935,
+  },
+  {
+    id: "MYS",
+    value: 991644,
+  },
+  {
+    id: "NAM",
+    value: 701897,
+  },
+  {
+    id: "NCL",
+    value: 144098,
+  },
+  {
+    id: "NER",
+    value: 312944,
+  },
+  {
+    id: "NGA",
+    value: 862877,
+  },
+  {
+    id: "NIC",
+    value: 90831,
+  },
+  {
+    id: "NLD",
+    value: 281879,
+  },
+  {
+    id: "NOR",
+    value: 224537,
+  },
+  {
+    id: "NPL",
+    value: 322331,
+  },
+  {
+    id: "NZL",
+    value: 86615,
+  },
+  {
+    id: "OMN",
+    value: 707881,
+  },
+  {
+    id: "PAK",
+    value: 158577,
+  },
+  {
+    id: "PAN",
+    value: 738579,
+  },
+  {
+    id: "PER",
+    value: 248751,
+  },
+  {
+    id: "PHL",
+    value: 557292,
+  },
+  {
+    id: "PNG",
+    value: 516874,
+  },
+  {
+    id: "POL",
+    value: 682137,
+  },
+  {
+    id: "PRI",
+    value: 957399,
+  },
+  {
+    id: "PRT",
+    value: 846430,
+  },
+  {
+    id: "PRY",
+    value: 720555,
+  },
+  {
+    id: "QAT",
+    value: 478726,
+  },
+  {
+    id: "ROU",
+    value: 259318,
+  },
+  {
+    id: "RUS",
+    value: 268735,
+  },
+  {
+    id: "RWA",
+    value: 136781,
+  },
+  {
+    id: "ESH",
+    value: 151957,
+  },
+  {
+    id: "SAU",
+    value: 111821,
+  },
+  {
+    id: "SDN",
+    value: 927112,
+  },
+  {
+    id: "SDS",
+    value: 966473,
+  },
+  {
+    id: "SEN",
+    value: 158085,
+  },
+  {
+    id: "SLB",
+    value: 178389,
+  },
+  {
+    id: "SLE",
+    value: 528433,
+  },
+  {
+    id: "SLV",
+    value: 353467,
+  },
+  {
+    id: "ABV",
+    value: 251,
+  },
+  {
+    id: "SOM",
+    value: 445243,
+  },
+  {
+    id: "SRB",
+    value: 202402,
+  },
+  {
+    id: "SUR",
+    value: 972121,
+  },
+  {
+    id: "SVK",
+    value: 319923,
+  },
+  {
+    id: "SVN",
+    value: 728766,
+  },
+  {
+    id: "SWZ",
+    value: 379669,
+  },
+  {
+    id: "SYR",
+    value: 16221,
+  },
+  {
+    id: "TCD",
+    value: 101273,
+  },
+  {
+    id: "TGO",
+    value: 498411,
+  },
+  {
+    id: "THA",
+    value: 506906,
+  },
+  {
+    id: "TJK",
+    value: 613093,
+  },
+  {
+    id: "TKM",
+    value: 327016,
+  },
+  {
+    id: "TLS",
+    value: 607972,
+  },
+  {
+    id: "TTO",
+    value: 936365,
+  },
+  {
+    id: "TUN",
+    value: 898416,
+  },
+  {
+    id: "TUR",
+    value: 237783,
+  },
+  {
+    id: "TWN",
+    value: 878213,
+  },
+  {
+    id: "TZA",
+    value: 442174,
+  },
+  {
+    id: "UGA",
+    value: 720710,
+  },
+  {
+    id: "UKR",
+    value: 74172,
+  },
+  {
+    id: "URY",
+    value: 753177,
+  },
+  {
+    id: "USA",
+    value: 658725,
+  },
+  {
+    id: "UZB",
+    value: 550313,
+  },
+  {
+    id: "VEN",
+    value: 707492,
+  },
+  {
+    id: "VNM",
+    value: 538907,
+  },
+  {
+    id: "VUT",
+    value: 650646,
+  },
+  {
+    id: "PSE",
+    value: 476078,
+  },
+  {
+    id: "YEM",
+    value: 957751,
+  },
+  {
+    id: "ZAF",
+    value: 836949,
+  },
+  {
+    id: "ZMB",
+    value: 714503,
+  },
+  {
+    id: "ZWE",
+    value: 405217,
+  },
+  {
+    id: "KOR",
+    value: 171135,
+  },
+];
